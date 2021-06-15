f878cff86e2d5922ada39b0268f6b8ec16ed764d
kilik52 <kilik52@hotmail.com>
2021-06-15 23:01:02 +0800

    feat: property.ui.startLevel

--- src/property.ts
+++ src/property.ts
@@ -47,5 +47,6 @@ export interface PropertyType {
     editable?: boolean;
     name?: boolean; // 是不是可以在UI上认为是name属性
     type?: "file"; // 虽然是primal_type是string，但是在表现上，是一个文件。要通过extractContentFromFile进行转化
+    startLevel?: string; // 这个是给HierarchySchema用的。在前端用Cascader来选择Object的时候，起始的一个level。一定要配合property自身的level属性才可以生效。
   };
 }
